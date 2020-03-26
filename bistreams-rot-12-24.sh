8631cb37a1979612a6ca73ff4f5460e51c6a3118
izm1chael <m1chael.johnson1804@gmail.com>
2020-03-26 12:01:34 +0000

    Create bistreams-rot-12-24.sh

---
+++ bistreams-rot-12-24.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Compress bistream files older than 3 Hours
+find /opt/dionaea/var/lib/dionaea/bistreams/* -type f -mmin +1140 -exec gzip {} $
+
+# Clear bistream logs from dionaea every 6 Hours
+find /opt/dionaea/var/lib/dionaea/bistreams/* -type f -mtime +1 -exec rm -rf {$
+
+# Clear bistream folder When it is empty 
+find /opt/dionaea/var/lib/dionaea/bistreams/* -type d -empty -delete
