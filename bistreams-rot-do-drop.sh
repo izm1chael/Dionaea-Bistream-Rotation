604be597066b30823dad7ecbcb15ed13c7c36287
izm1chael <m1chael.johnson1804@gmail.com>
2020-03-26 12:13:46 +0000

    Create bistreams-rot-do-drop.sh

---
+++ bistreams-rot-do-drop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Stop Dionaea before running
+supervisorctl start dionaea
+
+# Compress bistream files older than 3 Hours
+find /opt/dionaea/var/lib/dionaea/bistreams/* -type f -mmin +360 -exec gzip {} $
+
+# Clear bistream logs from dionaea every 6 Hoursk
+find /opt/dionaea/var/lib/dionaea/bistreams/* -type f -mmin +720 -exec rm -rf {$
+
+# Clear bistream folder When it is empty 
+find /opt/dionaea/var/lib/dionaea/bistreams/* -type d -empty -delete
+
+# Start Dionaea before closing
+
