7c870534e612468a426dac5e2636a32138b56e5d
izm1chael <m1chael.johnson1804@gmail.com>
2020-03-27 16:28:44 +0000

    Update bistreams-rot-3-6.sh

--- bistreams-rot-3-6.sh
+++ bistreams-rot-3-6.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Compress bistream files older than 3 Hours
-find /opt/dionaea/var/lib/dionaea/bistreams/* -type f -mmin +360 -exec gzip {} $
+find /opt/dionaea/var/lib/dionaea/bistreams/* -type f -mmin +360 -exec gzip {} \;
 
 # Clear bistream logs from dionaea every 6 Hours
-find /opt/dionaea/var/lib/dionaea/bistreams/* -type f -mmin +720 -exec rm -rf {$
+find /opt/dionaea/var/lib/dionaea/bistreams/* -type f -mmin +720 -exec rm -rf {} \;
 
 # Clear bistream folder When it is empty 
 find /opt/dionaea/var/lib/dionaea/bistreams/* -type d -empty -delete
